a633eee33e2e5f9adf350cc36ec634657b91b878
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-15 16:04:08 -0300

    500 até 1.c

--- 500 até 1.c
+++ 500 até 1.c
@@ -5,6 +5,7 @@ int main(){
 	setlocale(LC_ALL, "Portuguese");
 	int cont = 500;
 	printf("500 até 1:\n");
+	//Laço de repetição para exibir a resposta
 	while(cont >= 1){
 		printf("%i ", cont);
 		cont --;
