6d331f4a26bd563cca03cf7a2f76d87e6b6d71d1
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-15 14:29:59 -0300

    Update and rename l7q5.c to Médias e conceitos de 10 alunos.c

--- Médias e conceitos de 10 alunos.c
+++ Médias e conceitos de 10 alunos.c
@@ -1,12 +1,12 @@
 #include<stdio.h> 
 #include<locale.h>
-//Médias e conceitos de 10 alunos
+//MÃ©dias e conceitos de 10 alunos
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	float n1, n2, n3, m; 
 	int cont;
 	for(cont = 1; cont <= 10; cont ++){
-		printf("%iº aluno:\n", cont);
+		printf("%iÂº aluno:\n", cont);
 		printf("Digite a primeira nota:\n");
 		scanf("%f", &n1);
 		printf("Digite a segunda nota:\n");
@@ -15,19 +15,19 @@ int main(){
 		scanf("%f", &n3);
 		m = (n1 + n2 + n3) / 3;
 		if(m >= 9){
-			printf("Sua média foi %.1f, e o conceito obtido foi A.\n", m);
+			printf("Sua mÃ©dia foi %.1f, e o conceito obtido foi A.\n", m);
 		}
 		else if(m >= 7 && m < 9){
-			printf("Sua média foi %.1f, e o conceito obtido foi B.\n", m);
+			printf("Sua mÃ©dia foi %.1f, e o conceito obtido foi B.\n", m);
 		}
 		else if(m >= 6 && m < 7){
-			printf("Sua média foi %.1f, e o conceito obtido foi C.\n", m);
+			printf("Sua mÃ©dia foi %.1f, e o conceito obtido foi C.\n", m);
 		}
 		else if(m >= 4 && m < 6){
-			printf("Sua média foi %.1f, e o conceito obtido foi D.\n", m);
+			printf("Sua mÃ©dia foi %.1f, e o conceito obtido foi D.\n", m);
 		}
 		else{
-			printf("Sua média foi %.1f, e o conceito obtido foi E.\n", m);
+			printf("Sua mÃ©dia foi %.1f, e o conceito obtido foi E.\n", m);
 		}
 	}
 	return(0);
